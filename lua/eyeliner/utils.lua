7bcd130c10330e21d491729581708ce71e07f436
Jinho Yoon <yoonjinho0318@gmail.com>
2022-07-12 11:35:13 -0400

    ✨ Add detection of capital letters

--- lua/eyeliner/utils.lua
+++ lua/eyeliner/utils.lua
@@ -1,27 +1,14 @@
 local M = {}
 
---[[
-local delims = {
-  ['.'] = true, [' '] = true,
-  ['('] = true, [')'] = true,
-  ['['] = true, [']'] = true,
-  ['{'] = true, ['}'] = true,
-  ['='] = true, [','] = true,
-  [':'] = true, ['_'] = true,
-  ['+'] = true, ['-'] = true,
-  ['*'] = true, ['/'] = true,
-  ["'"] = true, ['"'] = true,
-  ["<"] = true, ['>'] = true,
-  ["#"] = true
-}
---]]
-
--- todo: capital letters
 local alphas = {
   a = true, b = true, c = true, d = true, e = true, f = true, g = true,
   h = true, i = true, j = true, k = true, l = true, m = true, n = true,
   o = true, p = true, q = true, r = true, s = true, t = true, u = true,
   v = true, w = true, x = true, y = true, z = true,
+  A = true, B = true, C = true, D = true, E = true, F = true, G = true,
+  H = true, I = true, J = true, K = true, L = true, M = true, N = true,
+  O = true, P = true, Q = true, R = true, S = true, T = true, U = true,
+  V = true, W = true, X = true, Y = true, Z = true,
 }
 
 --- check if char is an alphabetical character
