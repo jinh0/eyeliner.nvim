f13c8a19bd23db6ead7424b29f064279f3546738
Jinho <yoonjinho0318@gmail.com>
2022-08-22 14:06:05 -0400

    feat: add debug config

--- lua/eyeliner/config.lua
+++ lua/eyeliner/config.lua
@@ -2,13 +2,18 @@
 local M = {}
 
 local defaults = {
-  highlight_on_key = false
+  highlight_on_key = false,
+  debug = false
 }
 
 M.opts = defaults
 
 M.setup = function(opt)
   M.opts = vim.tbl_deep_extend('force', {}, defaults, opt or {})
+
+  if M.opts.debug then
+    vim.notify("DEBUG: " .. vim.inspect(M.opts))
+  end
 end
 
 return M
