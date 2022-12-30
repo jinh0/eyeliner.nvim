71ee41d105fc2ca1e8ee65c00bf3b65c7cf94969
jemag <alexandre.bd@tutanota.com>
2022-12-30 12:11:48 -0500

    fix: handle plugin/ being loaded before setup() is called
    For plugin managers like Lazy.nvim, `init.lua` and `plugin/` will be loaded
    first, and then setup() will be called. This causes problems currently
    since enable is already called without the user's configuration.

--- lua/eyeliner/config.lua
+++ lua/eyeliner/config.lua
@@ -9,11 +9,20 @@ local defaults = {
 M.opts = defaults
 
 M.setup = function(opt)
+  local eyeliner = require('eyeliner')
+  local enabled = require('eyeliner.view').enabled
+
+  if enabled then
+    eyeliner.disable()
+  end
+
   M.opts = vim.tbl_deep_extend('force', {}, defaults, opt or {})
 
   if M.opts.debug then
-    vim.notify("DEBUG: " .. vim.inspect(M.opts))
+    vim.notify('DEBUG: ' .. vim.inspect(M.opts))
   end
+
+  eyeliner.enable()
 end
 
 return M
--- plugin/init.lua
+++ plugin/init.lua
@@ -3,5 +3,4 @@ local eyeliner = require('eyeliner')
 vim.api.nvim_create_user_command('EyelinerEnable', eyeliner.enable, {})
 vim.api.nvim_create_user_command('EyelinerDisable', eyeliner.disable, {})
 vim.api.nvim_create_user_command('EyelinerToggle', eyeliner.toggle, {})
-
-eyeliner.enable()
+eyeliner.setup()
