390f8ea0d42238ecbf2d31b6d4a75bf6d89dddd9
Jinho Yoon <jinho.yoon@mail.mcgill.ca>
2024-08-03 13:54:23 -0400

    revert: revert on-key disabled filetypes
    This reverts commit c6fc2b614fb1f4d788f18d0452e5f2ed3186d9f1.
    
    revert: remove on-key disabled filetypes feature
    
    This reverts commit 5cf077fcfae362c09b52513cc49aa5a825af6da2.

--- lua/eyeliner/on-key.lua
+++ lua/eyeliner/on-key.lua
@@ -7,8 +7,6 @@ local ns_id = _local_3_["ns-id"]
 local clear_eyeliner = _local_3_["clear-eyeliner"]
 local apply_eyeliner = _local_3_["apply-eyeliner"]
 local dim = _local_3_["dim"]
-local disable_filetypes = _local_3_["disable-filetypes"]
-local disable_buftypes = _local_3_["disable-buftypes"]
 local utils = require("eyeliner.utils")
 local function on_key(key)
   local line = utils["get-current-line"]()
@@ -32,38 +30,23 @@ local function on_key(key)
   clear_eyeliner(y)
   return key
 end
-local function enable_keybinds()
-  if not vim.b[vim.api.nvim_get_current_buf()].eyelinerDisabled then
-    for _, key in ipairs({"f", "F", "t", "T"}) do
-      local function _7_()
-        return on_key(key)
-      end
-      vim.keymap.set({"n", "x", "o"}, key, _7_, {expr = true, buffer = 0})
-    end
-    return nil
-  else
-    return nil
-  end
-end
-local function remove_keybinds()
-  if not vim.b[vim.api.nvim_get_current_buf()].eyelinerDisabled then
-    for _, key in ipairs({"f", "F", "t", "T"}) do
-      vim.keymap.del({"n", "x", "o"}, key, {buffer = 0})
-    end
-    return nil
-  else
-    return nil
-  end
-end
 local function enable()
   if opts.debug then
     vim.notify("On-keypress mode enabled")
   else
   end
-  disable_filetypes()
-  disable_buftypes()
-  enable_keybinds()
-  utils["set-autocmd"]({"BufEnter", "BufWinEnter"}, {callback = enable_keybinds, group = "Eyeliner"})
-  return utils["set-autocmd"]({"BufLeave"}, {callback = remove_keybinds, group = "Eyeliner"})
+  for _, key in ipairs({"f", "F", "t", "T"}) do
+    local function _8_()
+      return on_key(key)
+    end
+    vim.keymap.set({"n", "x", "o"}, key, _8_, {expr = true})
+  end
+  return nil
+end
+local function remove_keybinds()
+  for _, key in ipairs({"f", "F", "t", "T"}) do
+    vim.keymap.del({"n", "x", "o"}, key)
+  end
+  return nil
 end
 return {enable = enable, ["remove-keybinds"] = remove_keybinds}
